ae7efe4691e9b29e7b6d74f87b232546fc850509
Vadim Korabelnik <vadimkorabelnik@yandex.ru>
2023-11-23 21:48:07 +0300

    Implemented computeSlug method

--- guestbook/src/Entity/Conference.php
+++ guestbook/src/Entity/Conference.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[ORM\Entity(repositoryClass: ConferenceRepository::class)]
 #[UniqueEntity('slug')]
@@ -42,9 +43,16 @@ class Conference
         return $this->city . ' ' . $this->year;
     }
 
-  public function getId(): ?int
+    public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
     }
 
     public function getCity(): ?string
